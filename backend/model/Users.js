4ec2c2f40289fce8b465a2397cca15a09ee37092
manuel <manu@correo.com>
2020-05-19 12:30:26 -0600

    esquema de usuarios

---
+++ backend/model/Users.js
@@ -0,0 +1,59 @@
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+const {imagesSchema}=require('./Images');
+
+const usersSchema=new mongoose.Schema({
+    first_name:{
+        type:String,
+        required:true,
+    },
+    last_name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    is_active:{
+        type:Boolean,
+        default:true,
+    },
+    password:{
+        type:String,
+    },
+    profile_img:{
+        type:String
+    },
+    images:[imagesSchema],
+    videos:[{
+        url:String
+    }]
+});
+
+usersSchema.pre('save', function(next) {
+    const user = this;
+  
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+  
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+  
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+  
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+    });
+  });
+
+const Users=mongoose.model('User',usersSchema);
+module.exports=Users;
