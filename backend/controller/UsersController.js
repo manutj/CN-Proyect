5ea67961f8b06b52d5c58da1cb7efc666aa72ce3
manuel <manu@correo.com>
2020-05-19 12:28:49 -0600

    controlador de usuario

---
+++ backend/controller/UsersController.js
@@ -0,0 +1,98 @@
+const {UsersService} = require('../services');
+const utils=require('../utils');
+module.exports={
+    create:(req,res)=>{
+    UsersService.create(req.body)
+    .then(user=>res.status(201).send(user))
+    .catch(err=>res.status(400).send({message:'Error creando el usuario',err}));
+    },
+
+    find:(req,res)=>{
+        UsersService.find()
+        .then(user=>res.status(200).send(user))
+        .catch(err=>res.status(404).send({message:'Usuario no encontrado',err}));
+    },
+
+    findOne:(req,res)=>{
+        UsersService.findOne(req.params.id)
+        .then(user=>{res.status(200).send(user)})
+        .catch(err=>res.status(404).send({message:'Usuario no encontrado'}));
+    },
+
+    update:async(req,res)=>{
+        if(req.files){
+          const {profile}=req.files  
+          const upload=await utils.upload(profile.tempFilePath);
+          if(upload){
+              req.body.profile_img=upload.url
+              console.log(upload)
+            }
+        }
+        UsersService.update(req.params.id,req.body)
+        .then(user=>{res.status(200).send(user)})
+        .catch(err=>res.status(404).send({message:'No se pudo actualizar el usuario'}));
+    },
+
+    uploadPhoto:async(req,res)=>{
+        const { id }  = req.params;
+        try {
+            const user = await UsersService.findOne(id);
+            if(req.files){
+                const {photo}=req.files  
+                const upload=await utils.upload(photo.tempFilePath);
+                if(upload){
+                    req.body.images=upload.url;
+                  }
+            UsersService.uploadPhoto(user,req.body.images)
+              }
+            res.status(200).send('todo bien',user);
+          } catch (err) {
+            res.status(400).send({ message: 'Error adding image to user', err }); 
+          }
+    },
+
+    uploadVideo:async(req,res)=>{
+        if(req.files){
+          const {video}=req.files  
+          const upload=await utils.upload(video.tempFilePath);
+          if(upload){
+              req.body.videos.push(upload.url)
+              console.log(upload)
+            }
+        }
+        UsersService.update(req.params.id,req.body)
+        .then(user=>{res.status(200).send(user)})
+        .catch(err=>res.status(404).send({message:'No se pudo actualizar el usuario'}));
+    },
+
+    delete:(req,res)=>{
+        UsersService.update(req.params.id,{is_active:false})
+        .then(user=>{res.status(200).send('Usuario eliminado correctamente')})
+        .catch(err=>res.status(404).send({message:'Usuario no encontrado'}));
+    },
+
+    signup:(req,res)=>{
+        UsersService.create(req.body)
+        .then(user=>res.status(201).send({message:'signup-succesfull',user}))
+        .catch(err=>res.status(400).send({message:'Error singup',err}));
+        },
+
+    login: async(req,res)=>{
+        const { email, password } = req.body;
+        try {
+            const user = await UsersService.findByEmail(email);
+            if (!user) res.status(404).send({ message: 'User not found' });
+            const isMatch = UsersService.comparePasswords(password, user.password);
+            if (!isMatch) res.status(400).send({ message: 'Invalid credentials' });
+            const token=utils.createToken({
+                id:user._id,
+                name:user.first_name,
+                email:user.email
+            });
+            res.status(200).send({ message: "step inside, brother",token});
+          } catch (error) {
+            console.log(error);
+            res.status(400).send({ message: 'Error on login', error });
+          }
+    }
+}
