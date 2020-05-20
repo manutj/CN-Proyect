122a7f371c6681e39af7ebaa4a3a5d8c073bb896
manuel <manu@correo.com>
2020-05-20 16:21:04 -0600

    agregar  vistas de home y login (moldes)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 
+//Views
+import Login from './views/login';
+import Home from './views/home';
+
+//Components
+import Navigation from './components/navigaton/navigation'
+
+//MISC
+import {
+  BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import Navigation from './components/navigaton';
+
 function App() {
   return (
-    <React.StrictMode>
-      <h1>
-        <p>Hola desde app</p>
-      </h1>
-    </React.StrictMode>
-      
-    
+    <React.Fragment>
+    <Navigation></Navigation>
+    <Router>
+    <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+          <Home />
+          </Route>
+        </Switch>
+    </Router>
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/views/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Home = () => {
+    return ( 
+        <React.Fragment>
+         <h1>
+        <p>Hola desde app</p>
+      </h1>
+      </React.Fragment>
+     );
+}
+ 
+export default Home;
