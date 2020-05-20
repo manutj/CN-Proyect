9a44857a2db8ed055d6f44182c328588f8c4ee03
manuel <manu@correo.com>
2020-05-20 16:23:28 -0600

    agregar vista de login (molde)

---
+++ frontend/src/views/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const  Login= () => {
+    return ( 
+        <React.Fragment>
+        <h1>Hola desde Login</h1>
+        </React.Fragment>
+     );
+}
+ 
+export default Login ;
