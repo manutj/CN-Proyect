85e2da3a47928e34d0e8bef1ce0b90d3edfe5d91
manuel <manu@correo.com>
2020-05-20 16:22:33 -0600

    agregar componente de barra de navegacion

---
+++ frontend/src/components/navigaton/navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Navigation = () => {
+    return ( 
+        <React.Fragment>
+            <h2>Navbar</h2>
+        </React.Fragment>
+     );
+}
+ 
+export default Navigati
+<React.Fragment>
+    <h2>Navbar</h2>
+</React.Fragment>on;
