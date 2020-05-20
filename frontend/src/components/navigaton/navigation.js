1f11cf48af7996f377db4b10ddec6ce6c45919d8
manuel <manu@correo.com>
2020-05-20 17:31:38 -0600

    agregar barra de navegacion usando enrutamiento

--- frontend/src/components/navigaton/navigation.js
+++ frontend/src/components/navigaton/navigation.js
@@ -1,14 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
-const Navigation = () => {
-    return ( 
-        <React.Fragment>
-            <h2>Navbar</h2>
-        </React.Fragment>
-     );
-}
- 
-export default Navigati
-<React.Fragment>
-    <h2>Navbar</h2>
-</React.Fragment>on;
+const Example = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+  
+    const toggle = () => setIsOpen(!isOpen);
+  
+    return (
+        <Navbar 
+        className="navbar navbar-dark bg-dark" 
+        //style={{backgroundColor:"black"}}
+        light expand="md">
+          <NavbarBrand tag={Link} to="/">Mi aplicacion</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+              <NavLink tag={Link} to="/login/">Login</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink tag={Link} to="/signup">Signup</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+    );
+  }
+  
+  export default Example;
