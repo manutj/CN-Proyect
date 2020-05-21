efbfaa7c3041e6f6dbeed6bbd69f88888504a8f9
manuel <manu@correo.com>
2020-05-21 10:12:40 -0600

    rutas de vistas

---
+++ frontend/src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Route} from 'react-router-dom'
+import Home from './views/home';
+import Login from './views/login';
+import Signup from './views/signup';
+
+export default [
+<React.Fragment>
+    <Route exact path="/" component={Home}></Route>
+    <Route exact path="/login" component={Login}></Route>
+    <Route exact path="/signup" component={Signup}></Route>
+</React.Fragment>
+]
